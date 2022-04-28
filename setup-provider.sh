9c3c2154dce8ced53f2b14d0c49acf6cdabd08de
Hossein Salahi <salahi.hossein@gmail.com>
2022-04-28 16:03:03 +0200

    wip

--- setup-provider.sh
+++ setup-provider.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kubectl create -f crossplane-resources/providers/gcp/config/secret-gcp.yaml
-kubectl crossplane install configuration registry.upbound.io/xp/getting-started-with-gcp:v1.7.0
+kubectl create -f ~/.config/gcloud/secret-gcp.yaml
